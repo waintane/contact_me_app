9c657395376d5bafb2838bc83de8ac601ce2803b
Olivier Paradis-Tardif <olitarpar@hotmail.com>
2023-01-13 22:05:02 -0500

    pass data to the server

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const sendMail = require("./functions/sendMail");
 
 const app = express()
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,12 +12,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.on("sendEmail", () => sendMail("worked"));
 
 app.get("/sendMail", (req,res) => {
-    console.log("dude")
     app.emit("sendEmail");
 })
 
+app.post("/", (req,res) => {
 
+    const { parcel, email } = req.body
+    console.log(email)
 
+    if(!parcel) {
+        return res.status(400).send({status: "failed"})
+    }
+    res.status(200).send({status: "recieved"})
+})
 
 app.listen(PORT, () =>{
     console.log(`le serveur est actif dans le port ${PORT}`)
